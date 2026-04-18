cdb37626b24ee811f94149fc496640f4d1632b8c
Emina Zubetljak <ezubetljak1@etf.unsa.ba>
2026-04-18 18:27:12 +0200

    Set up backend and frontend scaffolding

---
+++ Project/backend/src/test/java/ba/unsa/si/docflow/DocflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.si.docflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
