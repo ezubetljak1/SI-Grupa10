9c0005bc379163f71b40bbd34e8a13af9a51baeb
Mirza <mmhalilovi1@etf.unsa.ba>
2026-05-19 18:36:12 +0200

    Controller fix

--- Project/backend/src/main/java/ba/unsa/si/docflow/controller/DashboardController.java
+++ Project/backend/src/main/java/ba/unsa/si/docflow/controller/DashboardController.java
@@ -1,15 +1,17 @@
 package ba.unsa.si.docflow.controller;
 
 import ba.unsa.si.docflow.dto.dashboard.DashboardResponse;
+import ba.unsa.si.docflow.entity.enums.RoleName;
 import ba.unsa.si.docflow.response.ApiResponse;
 
+import ba.unsa.si.docflow.security.CurrentUserService;
 import ba.unsa.si.docflow.service.dashboard.DashboardService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/dashboard")
@@ -17,9 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class DashboardController {
     private final DashboardService dashboardService;
 
+    private final CurrentUserService currentUserService;
+
     @GetMapping("/company")
-    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     public ApiResponse<DashboardResponse> getCompanyDashboard() {
+        currentUserService.requireAnyRole(RoleName.ADMIN, RoleName.MANAGER);
 
         return new ApiResponse<>(
                 "OK",
